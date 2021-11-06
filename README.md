9a855343f7abc4b25b9aa5c40c10793191f28ac9
Hakan Ozler <ozler.hakan@gmail.com>
2021-11-06 20:21:56 +0300

    support group by option after retrieving records with 0.0.2

--- README.md
+++ README.md
@@ -1,33 +1,60 @@
 # Solring
 
-Solring is an easy-to-use import tool from solr to local storage. By supporting various options we can create custom queries and save from a running Solr server to a file.    
+Solring is an easy-to-use import tool from solr to local storage. By supporting various options we can create custom
+queries and save from a running Solr server to a file.
 
 ## How it works
 
 ```
-$ pip install solring
+$ pip install solring==0.0.2
 
-$ solring --help                                                                                                                [20:30:23]
-usage: solring [-h] [--version] --url URL [--output OUTPUT] [--save_format {csv,txt}] --core CORE [--rows ROWS] [-fl FL] [-q Q] [-fq FQ] [--score] [--qt QT]
+$ solring --help  
+usage: Solring.py [-h] [--version] --url URL [--output OUTPUT]
+                  [--save_format {csv,txt}] --core CORE [--rows ROWS] [-fl FL]
+                  [-q Q] [-fq FQ] [--score] [--qt QT]
+                  {group} ...
 
 optional arguments:
   -h, --help            show this help message and exit
   --version             show program's version number and exit
-  --url URL, -u URL     The host:port of the running solr
+  --url URL, -u URL     The host:port of the running solr.
   --output OUTPUT, -o OUTPUT
-                        Output file name
+                        Output file name.
   --save_format {csv,txt}, -sf {csv,txt}
-                        File type of saved records
-  --core CORE, -c CORE  The core/collection in solr
-  --rows ROWS, -r ROWS  The number of row numbers returned
-  -fl FL                Field list to retrieve
-  -q Q                  Search query
-  -fq FQ                Filter queries
-  --score               Learn score of each record
-  --qt QT               solr request handle to query on, default is '/select'
+                        File type of saved records. Default is txt.
+  --core CORE, -c CORE  The core/collection in solr.
+  --rows ROWS, -r ROWS  The number of row numbers returned. By default, Solr
+                        returns 5 batches at a time to save records.
+  -fl FL                Field list to retrieve. By default, Solr returns the
+                        id field.
+  -q Q                  Search query. By default, Solr returns all records.
+  -fq FQ                Filter queries.
+  --score               Learn score of each record in a score field.
+  --qt QT               solr request handle to query on, default is '/select'.
+
+group command:
+  {group}               group help
 ```
 
-Create a custom query where the query we search for is 'boat', we have two filter queries, and we only need to know their ids and titles as follows:
+The group command parameters:
+
+```
+$ solring group --help
+usage: Solring.py group [-h] --group_fl GROUP_FL --group_agg
+                        {mean,min,max,count} --group_column GROUP_COLUMN
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --group_fl GROUP_FL   The field(s) we want to use to group by.
+  --group_agg {mean,min,max,count}
+                        Aggregation functions to use in group by. Default is
+                        count.
+  --group_column GROUP_COLUMN
+                        The field(s) where we want to aggregate.
+```
+
+Create a custom query where the query we search for is 'boat', we have two filter queries, and we only need to know
+their ids and titles as follows:
 
 ```
 solring --url http://127.0.0.1:8983\ 
@@ -35,10 +62,30 @@ solring --url http://127.0.0.1:8983\
  -fq "cabin:[6 TO *]" \
  -fq harbors:marmaris \
  -q boat \
- -fl id,title
+ -fl id,title,boat_size,group_id
 
 $ ls 
 output.txt
 ```
+
+Let's now aggregate the above request with group options. We can learn the min and max of boats_size of each group:
+
+```
+solring --url http://127.0.0.1:8983\ 
+ -c boats \
+ -fq "cabin:[6 TO *]" \
+ -fq harbors:marmaris \
+ -q boat \
+ -r 100 \
+ -fl id,title,boat_size,group_id \
+ -o groupby_boats \
+ group --group_agg min --group_agg max --group_column boat_size --group_fl group_id
+
+$ ls æ
+groupby_boats.txt
+```
+## LICENSE
+
+MIT
 
 
--- src/solring/Solring.py
+++ src/solring/Solring.py
@@ -2,7 +2,7 @@ import solr
 import sys
 import argparse
 
-from pandas import DataFrame
+from pandas import DataFrame, concat
 
 
 class Solring:
@@ -15,7 +15,32 @@ class Solring:
         self.qt = qt
         self.core = core
 
-    def execute(self, params: dict, output: str, save_format: str):
+    def next_batch(self, handler, response, _params):
+        """
+        Load the next set of matches.
+        """
+        assert response is not None, "response is None"
+
+        try:
+            start = int(response.results.start)
+        except AttributeError:
+            start = 0
+
+        start += len(response.results)
+        _params['start'] = start
+        return handler(
+            fields=_params.get('fl'),
+            **_params
+        )
+
+    def execute(self, params: dict, gparams: dict, output: str, save_format: str):
+        """
+        Query Solr request
+        :param params: the solr request parameters
+        :param gparams: the group request parameters
+        :param output: when records will be saved to a file, output is used.
+        :param save_format: the saved file format
+        """
         solr_connection = solr.Solr(f"{self.url + '/solr/' + self.core}",
                                     persistent=False,
                                     timeout=360,
@@ -26,28 +51,71 @@ class Solring:
             fields=params.get('fl'),
             **params
         )
+        assert response is not None, "solr response is not valid"
+        print(f"{response.numFound} records found.")
+        if response.numFound == 0:
+            sys.exit(0)
 
-        assert response is not None, "response is None"
+        result_list = [DataFrame(response.results)]
+        response = self.next_batch(search_handler, response, params)
+        while response:
+            print(f"retrieve next {params.get('rows')} batches at {response.results.start}")
+            result_list.append(DataFrame(response.results))
+            response = self.next_batch(search_handler, response, params)
 
+        self.save_data(output, params, gparams, result_list, save_format)
+
+    def save_data(self, output, params, gparams, result_list, save_format):
         file_name = f"{output}.{save_format}"
-        DataFrame(response.results).to_csv(path_or_buf=file_name, index=False)
+        if gparams.get('param_group_by', False):
+            agg_dict = {
+                field: gparams['param_group_agg'] for field in gparams['param_group_column']
+            }
+            concat(result_list) \
+                .groupby(gparams['param_group_fl']) \
+                .agg(agg_dict) \
+                .reset_index(drop=False) \
+                .to_csv(path_or_buf=file_name, index=False)
+        else:
+            concat(result_list).to_csv(path_or_buf=file_name, index=False)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--version', action='version', version='solring 0.0.1')
-    parser.add_argument("--url", "-u", required=True, help="The host:port of the running solr", type=str)
-    parser.add_argument("--output", "-o", required=False, default='output', help="Output file name", type=str)
-    parser.add_argument("--save_format", "-sf", required=False, default='txt', help="File type of saved records",
+    parser.add_argument('--version', action='version', version='solring 0.0.2')
+    parser.add_argument("--url", "-u", required=True, help="The host:port of the running solr.", type=str)
+    parser.add_argument("--output", "-o", required=False, default='output', help="Output file name.", type=str)
+    parser.add_argument("--save_format", "-sf", required=False, default='txt',
+                        help="File type of saved records. Default is txt.",
                         choices=['csv', 'txt'])
-    parser.add_argument("--core", "-c", required=True, help="The core/collection in solr", type=str)
-    parser.add_argument("--rows", "-r", required=False, help="The number of row numbers returned", default=5, type=int)
-    parser.add_argument("-fl", required=False, default='id', help="Field list to retrieve", type=str)
-    parser.add_argument("-q", required=False, default="*:*", help="Search query", type=str)
-    parser.add_argument("-fq", required=False, action='append', help="Filter queries", type=str)
-    parser.add_argument("--score", required=False, action='store_true', help="Learn score of each record")
+    parser.add_argument("--core", "-c", required=True, help="The core/collection in solr.", type=str)
+    parser.add_argument("--rows", "-r",
+                        required=False,
+                        help="The number of row numbers returned. " +
+                             "By default, Solr returns 5 batches at a time to save records.",
+                        default=5, type=int)
+    parser.add_argument("-fl", required=False, default='id',
+                        help="Field list to retrieve. By default, Solr returns the id field.", type=str)
+    parser.add_argument("-q", required=False, default="*:*",
+                        help="Search query. By default, Solr returns all records.", type=str)
+    parser.add_argument("-fq", required=False, action='append', help="Filter queries.", type=str)
+    parser.add_argument("--score", required=False, action='store_true',
+                        help="Learn score of each record in a score field.")
     parser.add_argument("--qt", default='/select', required=False, type=str,
-                        help="solr request handle to query on, default is '/select'")
+                        help="solr request handle to query on, default is '/select'.")
+    subparser_group = parser.add_subparsers(
+        title='group command',
+        dest='command_group',
+        help='group help'
+    )
+    parser_group = subparser_group.add_parser('group')
+    parser_group.add_argument("--group_fl", required=True, action='append',
+                              help="The field(s) we want to use to group by.")
+    parser_group.add_argument("--group_agg", required=True, action='append',
+                              choices=['mean', 'min', 'max', 'count'],
+                              help="Aggregation functions to use in group by. Default is count.")
+    parser_group.add_argument("--group_column", required=True, action='append',
+                              help="The field(s) we want to aggregate.")
 
     args = parser.parse_args(sys.argv[1:])
 
@@ -58,11 +126,24 @@ def main():
         'score': args.score,
         'fl': args.fl
     }
+    group_params = {}
+    if args.command_group == 'group':
+        group_params = {
+            'param_group_by': args.command_group,
+            'param_group_fl': args.group_fl,
+            'param_group_column': args.group_column,
+            'param_group_agg': args.group_agg
+        }
     if args.fq:
         solr_params['fq'] = args.fq
 
     solring = Solring(args.url, args.core, args.qt)
-    solring.execute(solr_params, output=args.output, save_format=args.save_format)
+    solring.execute(
+        solr_params,
+        group_params,
+        output=args.output,
+        save_format=args.save_format
+    )
 
 
 if __name__ == '__main__':
